4e1f920344e58545394eee04536bb60f12d2c921
Thomas Mathiot <mathiotthomas@gmail.com>
2022-11-27 19:18:48 +0100

    Fixing ISO and Setup script

--- build-iso.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir tmp_building
-cp Setup.ZC tmp_building
-cp -r ZOSAddPalettes tmp_building
-xorriso -joliet "on" -as mkisofs tmp_building/ -o ZOSAddPalettes.iso
-rm -rf tmp_building
